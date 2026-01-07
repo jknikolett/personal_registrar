521eea706c8e8a5a8bba550b63f4ea82c8bffc9b
Ócsai Nikolett <jknikolett@gmail.com>
2026-01-07 11:16:08 +0100

    Init
    Initialize SpringBoot with database connection parameters.

---
+++ src/main/java/hu/kesmarki/ocsain/personal_registrar/PersonalRegistrarApplication.java
@@ -0,0 +1,13 @@
+package hu.kesmarki.ocsain.personal_registrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalRegistrarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalRegistrarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/kesmarki/ocsain/personal_registrar/PersonalRegistrarApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.kesmarki.ocsain.personal_registrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalRegistrarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
